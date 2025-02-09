4ca59d08744db2181addf9ce47e358382b6d2bd7
Stanislau Schastny <stasschastny@gmail.com>
2025-02-09 22:24:21 +0100

    Fix 2,0 Lesson_17

--- src/test/java/tests/DropdownTest.java
+++ src/test/java/tests/DropdownTest.java
@@ -5,25 +5,48 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.util.List;
 
 public class DropdownTest {
+
     @Test
-    public void checkDropdownTest() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+    public void checkOptionsPresenceTest() {
         WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver.get("https://the-internet.herokuapp.com/dropdown");
         Select dropdown = new Select(driver.findElement(By.id("dropdown")));
         List<WebElement> options = dropdown.getOptions();
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertFalse(options.isEmpty(), "Список опций пуст!");
+        softAssert.assertAll();
+        driver.close();
+    }
+
+    @Test
+    public void checkFirstSelectTest() {
+        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        Select dropdown = new Select(driver.findElement(By.id("dropdown")));
+        SoftAssert softAssert = new SoftAssert();
         dropdown.selectByValue("1");
         WebElement firstOption = dropdown.getFirstSelectedOption();
         softAssert.assertEquals("Option 1", firstOption.getText());
+        softAssert.assertAll();
+        driver.close();
+    }
+
+
+    @Test
+    public void checkSecondSelectTest() {
+        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        Select dropdown = new Select(driver.findElement(By.id("dropdown")));
+        SoftAssert softAssert = new SoftAssert();
         dropdown.selectByValue("2");
         WebElement secondOption = dropdown.getFirstSelectedOption();
         softAssert.assertEquals("Option 2", secondOption.getText());
